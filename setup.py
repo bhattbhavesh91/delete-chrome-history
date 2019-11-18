5b0b4f66aca6bf5cc37dcd9048ef2d4b2c2bb99d
Bhavesh Bhatt <bhattbhavesh91@gmail.com>
2019-11-18 21:08:30 +0530

    modified setup description

--- setup.py
+++ setup.py
@@ -1,41 +1,6 @@
-"""
-# Deleting specific websites from Google Chrome
-
-## Introduction
-Google Chrome doesn't give you an option of deleting specific websites from the history. In order to delete a specific website, I created a simple package.
- 
-## Installation
-
-To install the package, use pip:
-
-```python
-pip install delete-chrome-history-py
-```
----
-
-## Usage
-
-```python
-
-# Import the necessary modules
-from os.path import expanduser
-from chrome_delete import delete_history
-
-# Provide the full Chrome DB Location
-home = expanduser("~")
-chrome_db = ".config/google-chrome/Default/History"
-full_db_location = home + chrome_db
-
-# Provide the website you want to delete
-website_to_be_deleted = "github"
-
-# Call the function
-delete_history(full_db_location, website_to_be_deleted)
-
-```
-"""
-
 import setuptools
+
+DESCRIPTION = "A simple package to selectively delete specific websites from Google Chrome History"
 
 setuptools.setup(
     name="delete-chrome-history-py",
@@ -43,7 +8,7 @@ setuptools.setup(
     author="Bhavesh Bhatt",
     author_email="bhattbhavesh91@gmail.com",
     description="Delete specific websites from Google Chrome using Python",
-    long_description=__doc__,
+    long_description=DESCRIPTION,
     long_description_content_type="text/markdown",
     py_modules=["chrome_delete"],
     url="https://github.com/bhattbhavesh91/delete-chrome-history",
