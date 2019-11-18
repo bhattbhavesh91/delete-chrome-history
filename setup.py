8abaefe003c29fa58783ae6aa32c74c0d0834507
Bhavesh Bhatt <bhattbhavesh91@gmail.com>
2019-11-18 19:46:58 +0530

    updated requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="delete-chrome-history-py",
-    version="0.1.6",
+    version="0.1.7",
     author="Bhavesh Bhatt",
     author_email="bhattbhavesh91@gmail.com",
     description="Delete specific websites from Google Chrome using Python",
     py_modules=["chrome_delete"],
     url="https://github.com/bhattbhavesh91/delete-chrome-history",
-    install_requires=["regex", "pysqlite"],
+    install_requires=[],
 )
